e0924a37b00bbd502d6a65493482218a96d9d77f
LeonardoF22 <leonardoferodrigues7@gmail.com>
2024-09-17 11:34:32 -0300

    ' '

--- olamundo/src/Main.java
+++
@@ -1,9 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        Produto coca = new Produto();
-        coca.codigo = 1;
-        coca.descricao = "coca cola";
-    }
-}
